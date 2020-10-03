7a1c00c402d5c43171ca91ea70d8fcee4a9458a0
rabu-nee <38685640+rabu-nee@users.noreply.github.com>
2020-10-03 19:58:44 +0200

    add lifebuoy

---
+++ Escape the Spoon/Assets/_Scripts/Lifebuoy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifebuoy : MonoBehaviour {
+    [SerializeField]
+    private int bitesLeft = 5;
+    [SerializeField]
+    private float speedBoostDuration = 5f;
+
+    [SerializeField]
+    [Range(1f, 10f)]
+    private float speedBoost = 2f;
+
+    [SerializeField]
+    private GameObject model;
+
+    public Vector2 GetSpeedBoost() {
+        if (bitesLeft <= 0) {
+            return new Vector2(1f, 0);
+        }
+        else {
+            bitesLeft--;
+            if (bitesLeft == 0)
+                model.SetActive(false);
+            return new Vector2(speedBoost, speedBoostDuration);
+        }
+    }
+}
--- Escape the Spoon/Assets/_Scripts/Player.cs
+++ Escape the Spoon/Assets/_Scripts/Player.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public float movementSpeed = 100f;
-
     [SerializeField]
     private Transform SpriteGroup;
     private Rigidbody rb;
+    private Lifebuoy lifebuoy;
 
     private float horizontalMovement;
     private float verticalMovement;
+    private float speedMultiplier = 1f;
 
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody>();
+        lifebuoy = GetComponentInChildren<Lifebuoy>();
         SpriteGroup.LookAt(Camera.main.transform);
     }
 
@@ -22,10 +24,25 @@ public class Player : MonoBehaviour {
     void Update() {
         horizontalMovement = Input.GetAxis("Horizontal");
         verticalMovement = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Fire1") && speedMultiplier == 1f) {
+            Vector2 boost = lifebuoy.GetSpeedBoost();
+            if (boost.x != 1) {
+                speedMultiplier = boost.x;
+                StartCoroutine(RevertSpeedboostAfterTime(boost.y));
+            }
+        }
+    }
+
+    private IEnumerator RevertSpeedboostAfterTime(float time) {
+        Debug.Log("Boost start");
+        yield return new WaitForSeconds(time);
+        speedMultiplier = 1f;
+        Debug.Log("Boost over");
     }
 
     private void FixedUpdate() {
         //rb.velocity = Vector3.Normalize(new Vector3(horizontalMovement, rb.velocity.y, verticalMovement)) * movementSpeed * Time.deltaTime;
-        rb.MovePosition(transform.position + Vector3.Normalize(new Vector3(horizontalMovement, 0, verticalMovement)) * movementSpeed * Time.deltaTime);
+        rb.MovePosition(transform.position + Vector3.Normalize(new Vector3(horizontalMovement, 0, verticalMovement)) * movementSpeed * speedMultiplier * Time.deltaTime);
     }
 }
