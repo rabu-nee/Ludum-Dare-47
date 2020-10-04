9786850bbf1cb3c8d144145e3dd1b4504f6a7273
rabu-nee <38685640+rabu-nee@users.noreply.github.com>
2020-10-04 12:36:22 +0200

    add fluid decrease

--- Escape the Spoon/Assets/_Scripts/Bowl.cs
+++ Escape the Spoon/Assets/_Scripts/Bowl.cs
@@ -2,8 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bowl : MonoBehaviour
-{
-    public float radius;
+public class Bowl : MonoBehaviour {
+    public float
+        bottomRadius,
+        currentTopRadius;
+    //height of the fluid
     public float fluidLevel;
+
+    public Transform spawnPos;
+    public int maxFloatingFruitLoops = 50;
+
+    public Transform fluidPlane;
+    public BoxCollider pseudoGround;
+
+    [SerializeField]
+    private GameObject[] fruitLoops;
+
+    private float initFluidLevel;
+    private float initTopRadius;
+
+    private IEnumerator Start() {
+        fruitLoops = new GameObject[maxFloatingFruitLoops];
+        initFluidLevel = fluidLevel;
+        initTopRadius = currentTopRadius;
+        fluidPlane.localScale = new Vector3(currentTopRadius*2, 0.1f, currentTopRadius*2);
+        fluidPlane.transform.position = new Vector3(0, fluidLevel, 0);
+        pseudoGround.size = new Vector3(currentTopRadius * 2, 0.2f, currentTopRadius * 2);
+        pseudoGround.transform.position = new Vector2(0, fluidLevel - pseudoGround.size.y - 0.5f);
+
+        for (int i = 0; i < maxFloatingFruitLoops; i++) {
+            GameObject go = ObjectPooler.SpawnFromPool("FRUITLOOP", spawnPos.position, Quaternion.identity);
+            go.GetComponent<Rigidbody>().AddForce(Vector3.back * 4f, ForceMode.VelocityChange);
+            fruitLoops[i] = go;
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    public void DecreaseFluidLevel(float value) {
+        fluidLevel = Mathf.Clamp(fluidLevel - value, 0, initFluidLevel);
+        if (fluidLevel > 0) {
+            currentTopRadius = fluidLevel / initFluidLevel * initTopRadius;
+            fluidPlane.localScale = new Vector3(currentTopRadius * 2, 0.1f, currentTopRadius * 2);
+            fluidPlane.transform.position = new Vector3(0, fluidLevel, 0);
+            pseudoGround.size = new Vector3(currentTopRadius * 2, 0.2f, currentTopRadius * 2);
+            pseudoGround.transform.position = new Vector2(0, fluidLevel - pseudoGround.size.y - 0.5f);
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        UnityEditor.Handles.DrawWireDisc(new Vector3(transform.position.x, fluidLevel, transform.position.z), Vector3.up, currentTopRadius);
+    }
 }
---
+++ Escape the Spoon/Assets/_Scripts/Object Pool/ObjectPooler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Singleton;
+
+public class ObjectPooler : Singleton<ObjectPooler> {
+
+    [System.Serializable]
+    public class Pool {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    [System.Serializable]
+    public class Reaction {
+        public GameObject prefab;
+        public int size = 3;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    // Use this for initialization
+    void Start() {
+
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools) {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++) {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                obj.transform.parent = this.transform;
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public static GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation) {
+
+        if (!Instance.poolDictionary.ContainsKey(tag)) {
+            return null;
+        }
+
+        GameObject objToSpawn = Instance.poolDictionary[tag].Dequeue();
+
+        objToSpawn.SetActive(true);
+        objToSpawn.transform.position = position;
+        objToSpawn.transform.rotation = rotation;
+
+
+        IPooledObject pooledObject = objToSpawn.GetComponent<IPooledObject>();
+        if (pooledObject != null) {
+            pooledObject.OnObjectSpawn();
+        }
+
+        Instance.poolDictionary[tag].Enqueue(objToSpawn);
+        return objToSpawn;
+    }
+
+}
